cdaa4e66a02554a0525dbd70fcaa4e84f67f94d2
bahlockay <bmesser@horizon.csueastbay.edu>
2025-12-02 17:49:08 -0800

    added components for calculator

---
+++ src/Calculator.java
@@ -0,0 +1,28 @@
+import javax.swing.*
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator extends JFrame implements ActionListener {
+   // components 
+    private JTextField display;
+    private JButton[] numberButtons;
+    private JButton[] functionButtons;
+    private JButton addButton, subButton, mulButton, divButton;
+    private JButton decButton, equButton, delButton, clrButton;
+    private JButton sinButton, cosButton, logButton;
+    private JButtton mPlusButton, mMinusButton, mRecallButton, mClearButton;
+    private JToggleButton themeButton;
+    private JPanel panel;
+
+    // variables
+
+
+
+
+
+    public Calculator() {
+        
+    }
+
+
+}
